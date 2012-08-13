bd0a101dad0d3636efafd3e3a4b5966f9a451e75
root <root@vps.server.com>
2012-08-13 20:05:23 +0400

    add dependes

--- test/testfs.js
+++
@@ -1,8 +0,0 @@
-var fs = require('fs');
-var iconv = require('iconv-lite');
-
-var result=fs.readFileSync("test/chinese.js");
-console.log(iconv.decode( result.toString(),"gbk"));
-
-result=fs.readFileSync("test/testchinese.js");
-console.log(result.toString());
